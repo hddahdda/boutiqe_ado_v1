e27cec1470acf34af77cfb570c97b03a23c384de
hddahdda <hedda9@hotmail.com>
2021-08-13 16:55:51 +0000

    updated shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -16,18 +16,19 @@ def bag_contents(request):
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
-            'quantity': product,
+            'quantity': quantity,
+            'product': product,
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
